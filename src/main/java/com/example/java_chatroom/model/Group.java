d1e17ceace1d169e41a10ed41b8df85bd6f04c6b
comradeNo11 <1263225716@qq.com>
2025-07-10 20:35:23 +0800

    新增一个group实体类

---
+++ src/main/java/com/example/java_chatroom/model/Group.java
@@ -0,0 +1,10 @@
+package com.example.java_chatroom.model;
+
+public class Group {
+    private int groupId;
+    private String groupName;
+    private int ownerId;
+    private int groupStatus;
+
+
+}
